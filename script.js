4ed745e1e8fc47b3f110f05266a69298cf34e411
KalebWithAK <kad9603@gmail.com>
2023-01-26 16:10:30 -0500

    mobile responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = () => {
+/*window.onload = () => {
   const toggle = document.querySelector('input')
   toggle.addEventListener('click', (e) => toggleTheme(e.target.checked))
-}
+}*/
 
 function toggleTheme(dark_theme) {
   if (dark_theme) {
